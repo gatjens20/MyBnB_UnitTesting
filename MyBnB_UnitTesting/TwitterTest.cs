ef789ec527816381eba5df68c7071d82d9f0f58d
Otto Gatjens Sancho <gatjens20@hotmail.com>
2012-11-24 10:56:06 -0600

    Otto Gatjens Sancho

---
+++ MyBnB_UnitTesting/TwitterTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MyBnb;
+using MyBnb.Logica_Negocio;
+
+namespace MyBnB_UnitTesting
+{
+    [TestFixture]
+    public class TwitterTest
+    {
+        Twitter twitterObj;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            twitterObj = new Twitter();
+        }
+
+        [Test]        
+        public void realizarComentario_Test()
+        {
+            twitterObj.realizarPublicacion("Se ha registrado una nueva propiedad");
+        }
+
+    }
+}
